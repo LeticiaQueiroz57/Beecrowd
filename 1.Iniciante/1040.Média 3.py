087aa928df983d4333a95564bbbd38d6834b4e0e
Letíca Oliveira de Queiroz <leticia.queiroz5754@gmail.com>
2023-03-22 11:11:46 -0300

    beecrowd

--- 1.Iniciante/1040.Média 3.py
+++ 1.Iniciante/1040.Média 3.py
@@ -1,20 +1,19 @@
-n1, n2, n3, n4 = map(float,input().split()) # 2, 3, 4, 1
-m = (n1*2 + n2*3 + n3*4 + n4*1)/10
+n1, n2, n3, n4 = map(float, input().split())
+m = (n1*2 + n2*3 + n3*4 + n4)/10
+
+print(f"Media: {m:.1f}")
 
 if (m>=7):
-    print("Media:","%.1f"%m)
     print("Aluno aprovado.")
 elif (m>=5 and m<7):
-    nota = float(input())
-    print("Media:","%.1f"%m)
     print("Aluno em exame.")
-    print("Nota do exame:","%.1f"%nota)
-    media = (nota + m)/2
-    if (media>=5.0):
+    nota = float(input())
+    print(f"Nota do exame: {nota:.1f}")
+    media = (nota+m)/2
+    if (media>=5):
         print("Aluno aprovado.")
     else:
         print("Aluno reprovado.")
-    print("Media final:","%.1f"%media)
+    print(f"Media final: {media:.1f}")
 else:
-    print("Media:","%.1f"%m)
     print("Aluno reprovado.")
